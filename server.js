bb0e1955011aef02d4f056ff2b37635f079abc47
Felipe Cardozo <pypelyncar@hotmail.com>
2015-05-17 10:20:20 -0500

    adds new functionality

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
 	
 	var filePath = '.' + request.url;
 	if (filePath == './')
-		filePath = './index.htm';
+		filePath = './index.html';
 		
 	var extname = path.extname(filePath);
 	var contentType = 'text/html';
